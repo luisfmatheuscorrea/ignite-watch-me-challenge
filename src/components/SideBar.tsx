0a3df26de69fe2d62f9fb660b6f08116ddab5204
luisfmatheuscorrea <luisfilipe.ac@gmail.com>
2021-03-16 21:08:50 -0300

    Desafio Finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { api } from '../services/api';
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Props {
+  selectedGenreId: number;
+  handleClickButton(id: number): void;
+}
+
+export function SideBar({selectedGenreId, handleClickButton}: Props) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
